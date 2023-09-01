96578bc1691a230845f8f19a54ea812038dbaddf
unknown <me@peterhogler.dev>
2023-09-01 14:57:52 +0200

    added form data

--- Forum-grupp/src/views/CreateThreadPage.tsx
+++ Forum-grupp/src/views/CreateThreadPage.tsx
@@ -1,5 +1,35 @@
 const CreateThreadPage = () => {
-    return <div>Create Thread</div>;
+    const handleFormSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        const subjectValue = e.currentTarget.subject.value;
+        const descriptionValue = e.currentTarget.description.value;
+
+        const currentDateAndTime = new Date();
+        const formattedDateAndTime = currentDateAndTime.toISOString();
+
+        const thread = {
+            date: formattedDateAndTime,
+            subject: subjectValue,
+            description: descriptionValue,
+        };
+
+        console.log(thread);
+    };
+
+    return (
+        <form onSubmit={handleFormSubmit}>
+            <div>
+                <label htmlFor="subject">Subject</label>
+                <input type="text" name="subject" />
+            </div>
+            <div>
+                <label htmlFor="description">Description</label>
+                <input type="text" name="description" />
+            </div>
+            <button type="submit">Create Thread</button>
+        </form>
+    );
 };
 
 export default CreateThreadPage;
