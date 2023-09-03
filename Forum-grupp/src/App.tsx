316e8eeaf660941ad09db20753199dc90f838861
Frawser <mikis011@hotmail.com>
2023-09-03 19:26:01 +0200

    basic ts

--- Forum-grupp/src/App.tsx
+++ Forum-grupp/src/App.tsx
@@ -4,13 +4,18 @@ import { Link } from "react-router-dom";
 import ThreadList from "./components/ThreadList";
 
 function App() {
-    return (
-        <div className="App">   
-            <Link to={"/create-thread"}>Create Post</Link>
-            <ThreadList />
-        </div>
-
-    );
+  return (
+    <div className="App">
+      <Link to={"/create-thread"}>Create Post</Link>
+      <ThreadList
+        title="This is a title"
+        content="This is the content"
+        user="User"
+        date="2023-9-3"
+        comments={4}
+      />
+    </div>
+  );
 }
 
 export default App;
